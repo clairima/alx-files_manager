e7fc00e585943620dcda9356f6f3d93b4b6db3cb
clairima <mertahrima@gmail.com>
2024-03-12 23:54:35 +0100

    task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import { createClient } from 'redis';
 
 
@@ -8,4 +9,36 @@ class RedisClient {
         console.log(`Redis client not connected to server: ${error}`);
         });
     }
+
+
+    isAlive() {
+        if (this.client.connected) {
+          return true;
+        }
+        return false;
+      }
+    
+    
+    async get(key) {
+        const redisGet = promisify(this.client.get).bind(this.client);
+        const value = await redisGet(key);
+        return value;
+    }
+    
+
+    async set(key, value, time) {
+        const redisSet = promisify(this.client.set).bind(this.client);
+        await redisSet(key, value);
+        await this.client.expire(key, time);
+    }
+    
+
+    async del(key) {
+        const redisDel = promisify(this.client.del).bind(this.client);
+        await redisDel(key);
+      }
 }
+    
+const redisClient = new RedisClient();
+    
+module.exports = redisClient;
